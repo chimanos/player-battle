749747423726b9099e464e762caf136f1b5cb0ba
Romain Pamart <romain.pamart@yahoo.fr>
2018-12-28 00:33:29 +0100

    fix(Character): fix updateScore

--- src/main/kotlin/com/pamartromain/playerbattle/character/ressource/CharacterRessource.kt
+++ src/main/kotlin/com/pamartromain/playerbattle/character/ressource/CharacterRessource.kt
@@ -2,26 +2,41 @@ package com.pamartromain.playerbattle.character.ressource
 
 import com.pamartromain.playerbattle.character.entity.Character
 import com.pamartromain.playerbattle.character.service.CharacterService
+import com.pamartromain.playerbattle.player.entity.Player
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping(" character")
+@RequestMapping("character")
 class CharacterRessource {
 
     @Autowired
     private lateinit var characterService: CharacterService
 
     @PostMapping("/{name}/{score}/{playerId}")
-    fun addCharacter(@PathVariable(value = "name") name: String, @PathVariable(value = "score") score: Int, @PathVariable(value = "playerId") playerId: String)
-            = characterService.addCharacter(name, score, playerId)
+    fun addCharacter(@PathVariable(value = "name") name: String, @PathVariable(value = "score") score: Int, @PathVariable(value = "playerId") playerId: String): ResponseEntity<Player> {
+        val player = characterService.addCharacter(name, score, playerId)
+        return if(player != null) {
+            ResponseEntity(player, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
 
     @PutMapping("/{characterId}/{score}/{playerId}")
-    fun updateCharacterScore(@PathVariable(value = "characterId") characterId: String, @PathVariable(value = "score") score: Int, @PathVariable(value = "playerId") playerId: String)
-            = characterService.updateCharacterScore(characterId, score, playerId)
+    fun updateCharacterScore(@PathVariable(value = "characterId") characterId: String, @PathVariable(value = "score") score: Int, @PathVariable(value = "playerId") playerId: String): ResponseEntity<Player> {
+        val player = characterService.updateCharacterScore(characterId, score, playerId)
+        return if(player != null) {
+            ResponseEntity(player, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
 
-    @DeleteMapping
-    fun deleteCharacter(@RequestBody character: Character, playerId: String) = characterService.deleteCharacter(character, playerId)
+    @DeleteMapping("/{characterId}/{playerId}")
+    fun deleteCharacter(@PathVariable(value = "characterId") characterId: String, @PathVariable(value = "playerId") playerId: String) = characterService.deleteCharacter(characterId, playerId)
 
     @GetMapping("/{playerId}")
     fun getAllCharacters(@PathVariable(value = "playerId") playerId: String) = characterService.getAllCharacters(playerId)
--- src/main/kotlin/com/pamartromain/playerbattle/character/service/CharacterService.kt
+++ src/main/kotlin/com/pamartromain/playerbattle/character/service/CharacterService.kt
@@ -1,6 +1,7 @@
 package com.pamartromain.playerbattle.character.service
 
 import com.pamartromain.playerbattle.character.entity.Character
+import com.pamartromain.playerbattle.player.entity.Player
 import com.pamartromain.playerbattle.player.service.PlayerService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -13,38 +14,79 @@ class CharacterService {
     @Autowired
     private lateinit var playerService: PlayerService
 
-    fun addCharacter(name: String, score: Int, playerId: String): ArrayList<Character> {
+    fun addCharacter(name: String, score: Int, playerId: String): Player? {
         val player = playerService.getPlayerById(playerId)
-        val character = Character("$name${UUID.randomUUID()}", name, score)
 
-        if(player!!.characters != null) {
-            player.characters!!.add(character)
+        if((player!!.wallet - score) < 0) {
+            return null
         } else {
-            player!!.characters = ArrayList<Character>()
-            player.characters!!.add(character)
+            val character = Character("$name${UUID.randomUUID()}", name, score)
+
+            if(player!!.characters != null) {
+                player.characters!!.add(character)
+            } else {
+                player!!.characters = ArrayList<Character>()
+                player.characters!!.add(character)
+            }
+
+            player.wallet = player.wallet - score
+
+            playerService.updatePlayer(player)
         }
 
-        return player.characters!!
+        return player
     }
 
-    fun updateCharacterScore(characterId: String, score: Int, playerId: String): ArrayList<Character> {
+    fun updateCharacterScore(characterId: String, score: Int, playerId: String): Player? {
         val player = playerService.getPlayerById(playerId)
 
         player!!.characters!!.forEach {
             if(it.characterId == characterId) {
-                it.score = score
+                if(score > it.score) {
+                    val finalSub = score-it.score
+
+                    if((player.wallet - finalSub) < 0) {
+                        return null
+                    } else {
+                        it.score = score
+                        player.wallet = player.wallet - finalSub
+
+                        playerService.updatePlayer(player)
+                    }
+                }
+                if(score < it.score) {
+                    val finalAdd = it.score-score
+
+                    it.score = score
+                    player.wallet = player.wallet + finalAdd
+
+                    playerService.updatePlayer(player)
+                }
             }
         }
 
-        return player.characters!!
+        return player
     }
 
-    fun deleteCharacter(character: Character, playerId: String): ArrayList<Character> {
+    fun deleteCharacter(characterId: String, playerId: String): Player {
         val player = playerService.getPlayerById(playerId)
 
-        player!!.characters!!.remove(character)
+        var character: Character? = null
 
-        return player.characters!!
+        player!!.characters!!.forEach {
+            if(it.characterId == characterId) {
+                character = it
+            }
+        }
+
+        if(character != null) {
+            player.wallet = player.wallet + character!!.score
+            player.characters!!.remove(character!!)
+
+            playerService.updatePlayer(player)
+        }
+
+        return player
     }
 
     fun getAllCharacters(playerId: String): ArrayList<Character> {
